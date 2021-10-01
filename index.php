57d790d44d143cfa2c988d665d3a6166e929983d
Vladislavtl <vladtopol345@gmail.com>
2021-10-01 13:04:00 +0300

    second commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<h1>Про нас</h1>
+
+<form action="check_post.php" method="post">
+
+	<input type="text" name="username" placeholder="Введите имя"></br>
+	<input type="email" name="email" placeholder="Введите email"></br>
+	<input type="password" name="password" placeholder="Введите пароль"></br>
+	<input type="submit" value="Отправить">
+		
+</form>
+
+
+<?php
+
+echo "hello vlad";
